fb98f06e815a061a14d262384e005d8276af02d1
shrisoundharyaa <shrisoundharyaa@gmail.com>
2025-02-17 00:38:38 +0530

    created a navbar and some routes

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Project from "./routes/Project";
+
+import {Route, Routes} from "react-router-dom";
+function App() {
+  return (
+   <>
+   <Routes>
+    <Route  path ="/" element = {<Home />} />
+    <Route  path ="/project" element = {<Project />} />
+    <Route  path ="/contact" element = {<Contact />} />
+    <Route  path ="/about" element = {<About />} />
+   </Routes>
+   </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/HeroImg.js
@@ -0,0 +1,15 @@
+ import "./HeroImgStyles.css";
+import React from 'react'
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default HeroImg
