36e476c66eea82649e4fa343087e8e91d572bd73
shrisoundharyaa <shrisoundharyaa@gmail.com>
2025-02-18 22:56:39 +0530

    created a home page

--- frontend/src/components/HeroImg.js
+++ frontend/src/components/HeroImg.js
@@ -1,13 +1,27 @@
  import "./HeroImgStyles.css";
 import React from 'react'
-
+import IntroImg from "../assets/bg4.jpg";
+import  {Link} from "react-router-dom";
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            
+            <img className="intro-img"
+           src={IntroImg} alt = "IntroImg"
+           />
         </div>
-
+       <div className="content">
+        <p> HI, I'M A SOUNDHARYAA SHRI S</p>
+        <h1> FullStack Developer</h1>
+        <div> 
+          <Link to ="/project" className="btn">
+              Projects
+          </Link>
+          <Link to ="/contact" className="btn btn-light">
+              Contact
+          </Link>
+        </div>
+       </div>
     </div>
   )
 }
